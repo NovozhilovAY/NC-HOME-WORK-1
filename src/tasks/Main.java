1020c38238ace34d5fd23c4974ccaec9a96d589b
NovozhilovAY <sasha.n191@yandex.ru>
2021-10-16 22:06:36 +0300

    Circle realization

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -1,4 +1,16 @@
 package tasks;
 
+import tasks.task1.Circle;
+
 public class Main {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(15);
+        Circle circle2 = new Circle(15,"yellow");
+        Circle circle3 = new Circle();
+        System.out.println(circle1);
+        System.out.println(circle2);
+        System.out.println(circle3);
+
+        System.out.println(circle1.getArea());
+    }
 }
--- src/tasks/task1/Circle.java
+++ src/tasks/task1/Circle.java
@@ -1,4 +1,37 @@
 package tasks.task1;
 
 public class Circle {
+    private double radius = 1.0;
+    private String color = "red";
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String toString() {
+        return "Circle[" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ']';
+    }
+
+    public double getArea() {
+        return Math.PI * Math.pow(radius,2);
+    }
 }
