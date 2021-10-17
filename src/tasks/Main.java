0fa30be0fc0e19312609da6cf8d9cde8f30dd9c0
NovozhilovAY <sasha.n191@yandex.ru>
2021-10-17 10:26:43 +0300

    add Employee class

--- src/tasks/Main.java
+++ src/tasks/Main.java
@@ -2,6 +2,7 @@ package tasks;
 
 import tasks.task1.Circle;
 import tasks.task2.Rectangle;
+import tasks.task3.Employee;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,9 +13,17 @@ public class Main {
         System.out.println(circle2);
         System.out.println(circle3);
         System.out.println(circle1.getArea());
+
         Rectangle rectangle = new Rectangle(5,4);
         System.out.println(rectangle);
         System.out.println(rectangle.getPerimeter());
         System.out.println(rectangle.getArea());
+
+        Employee employee = new Employee(1, "John","Rambo",244);
+        System.out.println(employee);
+        System.out.println(employee.getAnnualSalary());
+        System.out.println(employee.raiseSalary(30));
+        System.out.println(employee);
+        System.out.println(employee.getAnnualSalary());
     }
 }
